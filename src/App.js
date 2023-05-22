df5c4f45227eb72bd96aeed1c0648be4709622c5
YogitaBhagat1996 <133939060+YogitaBhagat1996@users.noreply.github.com>
2023-05-22 11:30:13 +0530

    user defined function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App()
-{
-  return<h1>Hello React....!!!</h1>;
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+import MainBody from "./pages/MainBody";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <MainBody />
+      <Footer />
+    </div>
+  );
 }
 export default App;
---
+++ src/pages/MainBody.js
@@ -0,0 +1,12 @@
+function MainBody() {
+  return (
+    <div>
+      <p>
+        React lets you build user interfaces out of individual pieces called
+        components. Create your own React components like Thumbnail, LikeButton,
+        and Video. Then combine them into entire screens, pages, and apps.
+      </p>
+    </div>
+  );
+}
+export default MainBody;
