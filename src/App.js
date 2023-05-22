78faa30588bb1f81a6af2c9583820db29490d59f
YogitaBhagat1996 <133939060+YogitaBhagat1996@users.noreply.github.com>
2023-05-22 15:41:00 +0530

    interpolation

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ function App() {
   return (
     <div>
       <h1>Interpolation Demo</h1>
-      <InterpolationBasic />
+      <hr />
+      <InterpolationBasics />
     </div>
   );
 }
-function InterpolationBasic() {
+
+function InterpolationBasics() {
+  //1.  Data Member
+  let title = "Hello World";
+  let para = `DAC, KOCHI, TVDM ipsum dolor sit amet consectetur adipisicing elit. Pariatur, optio
+              repellendus delectus velit, est eaque nihil non, et alias dicta animi
+              asperiores ullam placeat odit autem sunt voluptates impedit repudiandae?`;
+  let price = 99.99;
+  let user = { id: 1, name: "rohit", email: "rohit@gmail.com" }; // object literal
+
+  //2. member function, coming soon...
+
+  // 3. UI / VIEW :: JSX
   return (
     <div>
-      <h1>Hello World....!!</h1>
-      <p>
-        React lets you build user interfaces out of individual pieces called
-        components. Create your own React components like Thumbnail, LikeButton,
-        and Video. Then combine them into entire screens, pages, and apps.
-      </p>
+      <h1>
+        {title} - {price}
+      </h1>
+      <h1>
+        {user.id} {user.name} {user.email}
+      </h1>
+      <h1> {JSON.stringify(user)} </h1>
+
+      <p>{para}</p>
     </div>
   );
 }
+
 export default App;
