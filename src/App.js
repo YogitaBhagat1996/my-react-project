93811f0fedeef1a85d3942b1749a72964afbea61
YogitaBhagat1996 <133939060+YogitaBhagat1996@users.noreply.github.com>
2023-05-22 14:41:31 +0530

    userdefinedtag

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 function App() {
-  //two tag so parenthesis is required
   return (
     <div>
       <h1>Hello World</h1>
-      <Onetag />
-      <Multitag />
+      <hr />
+      <Header />
+      <hr />
+      <Header />
     </div>
   );
 }
-function Onetag() {
-  return <h1>Hello React.....!!!!</h1>; //one line html so parenthesis is optional
-}
 
-function Multitag() {
-  //multilie html so parenthesis is required
+function Header() {
   return (
-    <h1>
-      React lets you build user interfaces out of individual pieces called
-      components. Create your own React components like Thumbnail, LikeButton,
-      and Video. Then combine them into entire screens, pages, and apps.
-    </h1>
+    <div>
+      <p>
+        React lets you build user interfaces out of individual pieces called
+        components. Create your own React components like Thumbnail, LikeButton,
+        and Video. Then combine them into entire screens, pages, and apps.
+      </p>
+    </div>
   );
 }
-
 export default App;
